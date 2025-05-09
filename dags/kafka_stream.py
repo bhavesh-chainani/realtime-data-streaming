1437bbc57089ecea61212c7cc2ea344c67fb5deb
bhavesh-chainani <bhaveshchainani@gmail.com>
2025-05-09 14:07:29 +0800

    Obtained original data.

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -9,10 +9,33 @@ default_args = {
     'start_date': datetime(2023, 9, 3, 10, 00)
 }
 
-def stream_data():
+def get_data():
     res = requests.get('https://randomuser.me/api/')
     res = res.json()['results'][0]
-    print(json.dumps(res, indent=3))
+    return res
+    # print(json.dumps(res, indent=3))
+
+def format_data(res):
+    data = {}
+    location = res['location']
+    data['first_name'] = res['name']['first']
+    data['last_name'] = res['name']['last']
+    data['gender'] = res['gender']
+    data['address'] = f"{str(location['street']['number'])} {location['street']['name']}, " \
+                      f"{location['city']}, {location['state']}, {location['country']}"
+    data['post_code'] = location['postcode']
+    data['email'] = res['email']
+    data['username'] = res['login']['username']
+    data['dob'] = res['dob']['date']
+    data['registered_date'] = res['registered']['date']
+    data['phone'] = res['phone']
+    data['picture'] = res['picture']['medium']
+    return data
+
+def stream_data():
+    res = get_data()
+    res = format_data(res)
+    print(json.dumps(res, indent=3, ensure_ascii = False))
 
 # with DAG('user_automation',
 #          default_args=default_args,
@@ -25,3 +48,4 @@ def stream_data():
 #     )
 
 stream_data()
+

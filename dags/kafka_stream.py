843e3ed0dfd634179b6f9177e0eaf7b7822fddbc
bhavesh-chainani <bhaveshchainani@gmail.com>
2025-05-14 11:40:45 +0800

    Updated code.

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -4,48 +4,52 @@ from airflow.operators.python import PythonOperator
 import json
 import requests
 
-default_args = {
-    'owner': 'airscholar',
-    'start_date': datetime(2023, 9, 3, 10, 00)
-}
+from kafka import KafkaProducer
+
+default_args = {"owner": "airscholar", "start_date": datetime(2023, 9, 3, 10, 00)}
+
 
 def get_data():
-    res = requests.get('https://randomuser.me/api/')
-    res = res.json()['results'][0]
+    res = requests.get("https://randomuser.me/api/")
+    res = res.json()["results"][0]
     return res
     # print(json.dumps(res, indent=3))
 
+
 def format_data(res):
     data = {}
-    location = res['location']
-    data['first_name'] = res['name']['first']
-    data['last_name'] = res['name']['last']
-    data['gender'] = res['gender']
-    data['address'] = f"{str(location['street']['number'])} {location['street']['name']}, " \
-                      f"{location['city']}, {location['state']}, {location['country']}"
-    data['post_code'] = location['postcode']
-    data['email'] = res['email']
-    data['username'] = res['login']['username']
-    data['dob'] = res['dob']['date']
-    data['registered_date'] = res['registered']['date']
-    data['phone'] = res['phone']
-    data['picture'] = res['picture']['medium']
+    location = res["location"]
+    data["first_name"] = res["name"]["first"]
+    data["last_name"] = res["name"]["last"]
+    data["gender"] = res["gender"]
+    data["address"] = (
+        f"{str(location['street']['number'])} {location['street']['name']}, "
+        f"{location['city']}, {location['state']}, {location['country']}"
+    )
+    data["post_code"] = location["postcode"]
+    data["email"] = res["email"]
+    data["username"] = res["login"]["username"]
+    data["dob"] = res["dob"]["date"]
+    data["registered_date"] = res["registered"]["date"]
+    data["phone"] = res["phone"]
+    data["picture"] = res["picture"]["medium"]
     return data
+
 
 def stream_data():
     res = get_data()
     res = format_data(res)
-    print(json.dumps(res, indent=3, ensure_ascii = False))
+    print(json.dumps(res, indent=3, ensure_ascii=False))
+
 
 # with DAG('user_automation',
 #          default_args=default_args,
 #          schedule_interval='@daily',
 #          catchup=False) as dag:
-    
+
 #     streaming_task = PythonOperator(
 #         task_id = 'stream_data_from_api',
 #         python_callable=stream_data
 #     )
 
-stream_data()
-
+# stream_data()
